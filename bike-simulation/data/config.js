f07fb2a6ce254bd218ec316d784846b3d9fba9b5
Gloria <gloria.palm7810@gmail.com>
2024-02-08 15:15:29 +0100

    bike simulator

---
+++ bike-simulation/data/config.js
@@ -0,0 +1,18 @@
+/**
+ * Bike rental simulation configuration data.
+ * Vteam 2023 Group 8.
+ */
+"use strict";
+
+const baseURL = "http://server:1337/api/v1"; // If run via Docker compose
+//const baseURL = "http://localhost:1337/api/v1";
+const numUsers = 300;                // Should be greater than sum of maxRidesCity
+const numBikesCity = [46, 375, 79];  // [Karlskrona, Stockholm, Helsingborg]
+const maxRidesCity = [30, 185, 35];  // Should be smaller than numBikesCity
+const minRideDuration = 60;    // seconds
+const minDistanceUpdate = 20;  // Min travel distance for bike to update database (m)
+const batteryDischarge = 0.02; // Percentage points discharge per meter
+const logRides = true;         // Write started and finished rides to console
+
+module.exports = { baseURL, numUsers, numBikesCity, maxRidesCity,
+    minRideDuration, minDistanceUpdate, batteryDischarge, logRides };
---
+++ bike-simulation/data/config2000.js
@@ -0,0 +1,18 @@
+/**
+ * Bike rental simulation configuration data.
+ * Vteam 2023 Group 8.
+ */
+"use strict";
+
+const baseURL = "http://server:1337/api/v1"; // If run via Docker compose
+//const baseURL = "http://localhost:1337/api/v1";
+const numUsers = 1300;                 // Should be greater than sum of maxRidesCity
+const numBikesCity = [182, 1502, 316]; // [Karlskrona, Stockholm, Helsingborg]
+const maxRidesCity = [120, 740, 140];  // Should be smaller than numBikesCity
+const minRideDuration = 60;    // seconds
+const minDistanceUpdate = 20;  // Min travel distance for bike to update database (m)
+const batteryDischarge = 0.02; // Percentage points discharge per meter
+const logRides = false;        // Write started and finished rides to console
+
+module.exports = { baseURL, numUsers, numBikesCity, maxRidesCity,
+    minRideDuration, minDistanceUpdate, batteryDischarge, logRides };
